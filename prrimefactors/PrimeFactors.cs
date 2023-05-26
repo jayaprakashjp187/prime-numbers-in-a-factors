713be4afb1acd4158d85e2e5f30e47bf12e608c7
jayaprakash <jayaprakashjp187@gmail.com>
2023-05-26 21:11:57 +0530

    finding prime numbers in a factors of number

---
+++ prrimefactors/PrimeFactors.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prrimefactors
+{
+    internal class PrimeFactors
+    {
+        public void prime(int x)
+        {
+            int num = x;
+            
+            
+           
+            for (int i=1;i<=num;i++)
+            {
+                if(num%i == 0)
+                {
+                    int count = 0;
+                    for (int j = 1; j <= i; j++)
+                    {
+
+                        if (i % j == 0)
+                        {
+                            count++;
+                        }
+                    }
+                        if (count == 2)
+                        {
+                            Console.WriteLine(i);
+                        }
+
+                    
+                }
+
+                
+
+               
+              
+
+
+            }
+           
+            
+
+
+        }
+    }
+}
---
+++ prrimefactors/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace prrimefactors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("enter the number");
+            int x=Convert.ToInt32(Console.ReadLine());
+            PrimeFactors p=new PrimeFactors();
+            p.prime(x);
+        }
+    }
+}
